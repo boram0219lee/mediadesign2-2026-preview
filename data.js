6617a24e9f50003ea54b26d8771f004996833cf9
boram0219lee <boram0219.lee@gmail.com>
2026-06-09 13:15:04 +0900

    Update data.js

--- data.js
+++ data.js
@@ -3,7 +3,7 @@ const works = [
     name: "강다은",
     title: "얼음 결정",
     description: "몽환적인 분위기 속에 깃든 얼음 결정 생명체",
-    link: "https://editor.p5js.org/daeunkang/sketches/vVQd4a_gI",
+    link: "https://editor.p5js.org/daeunkang/full/vVQd4a_gI",
     thumbnail: "강다은_얼음결정.jpg",
     input: "mouse",
     sound: true
