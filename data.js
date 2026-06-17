85dc0ead425c3c94e85a64b77fd1e90932f53c3c
boram0219lee <boram0219.lee@gmail.com>
2026-06-17 09:04:43 +0900

    Update data.js

--- data.js
+++ data.js
@@ -557,7 +557,7 @@ const works = [
     "rawOutput": "",
     "p5Original": "https://editor.p5js.org/yewon1189/full/vGogxsuRU",
     "thumbnailOriginal": "https://drive.google.com/open?id=1piDtNLiSgTS32px2jul8OjRu6JDRHtGK",
-    "cropTop": 58,
+    "cropTop": 0,
     "tags": [
       "마우스"
     ],
